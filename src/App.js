5f49c625797cf463bb57ff3d0341ab09310d23a0
fullstack290623 <fullstack290623@gmail.com>
2024-03-14 18:59:44 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello world!</h1>
-    </div>
-  );
+import React,{ Component } from 'react'
+import Car from './components/Car'
+class App extends Component {
+  render() {
+    // create component called HelloWolrd
+    // inside <h1>Hello world!<h1> in green
+    // line #10 <HelloWorld />
+    return (
+      <div className="App">
+        <h1>Hello world!</h1>
+        <Car />
+        <Car />
+        <Car />
+
+      </div>
+    );
+  }
 }
 
 export default App;
