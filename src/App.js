d4f66aaa8f621878e67a37153e293c3325dfa49b
fullstack290623 <fullstack290623@gmail.com>
2024-03-18 18:37:52 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react'
 import Garage from './components/Garage'
+import AddCar from './components/AddCar'
+import "./App.css"
+
 class App extends Component {
+  static my_car_seq = 4
   state = {
     cars: [
-      { brand: "Honda", model: "Civic", color: "green" , year: 2020, hybrid: true, id: 1},
+      { brand: "Honda", model: "Civic", color: "green", year: 2020, hybrid: true, id: 1 },
       { brand: "Ferrari", model: "Testa Rossa", color: "red", year: 2023, id: 2 },
       { brand: "Chevrolet", model: "Cicalvaliervic", color: "blue", year: 2024, id: 3 }
     ]
+  }
+  deleteCar = _id => {
+    const new_cars = this.state.cars.filter(car => car.id != _id)
+    this.setState({
+      cars: new_cars
+    })
+  }
+  addCar = _car => {
+      // apply the id to car (use my_car_seq)
+      // add the car to the array 
+      // set state
   }
   render() {
     // create component called HelloWolrd
@@ -14,8 +29,9 @@ class App extends Component {
     // line #10 <HelloWorld />
     return (
       <div className="App">
-        <h1>Hello world!</h1>
-        <Garage cars={this.state.cars} />
+        <h1>My Garage</h1>
+        <AddCar />
+        <Garage cars={this.state.cars} delete_car={this.deleteCar} />
       </div>
     );
   }
--- src/components/Garage.js
+++ src/components/Garage.js
@@ -3,18 +3,20 @@ import Car from './Car'
 
 class Garage extends Component {
     render() {
-        const { cars } = this.props;
-        const carsList = cars.filter(car => car.year > 2020).map(car => {
+        const { cars, delete_car } = this.props;
+        //const carsList = cars.filter(car => car.year > 2020).map(car => {
+        const carsList = cars.filter(car => car.year > 0).map(car => {
             return (
                 <div key={car.id}>
-                    <Car brand={car.brand} model={car.model} color={car.color} year={car.year}/>
+                    <Car brand={car.brand} model={car.model} color={car.color} year={car.year} delete_car={delete_car}
+                            id={car.id} />
                     <hr />
                 </div>
             )
         })
         return (
             <div>
-                { carsList }
+                {carsList}
             </div>
         )
     }
